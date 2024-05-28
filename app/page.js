e9c271cc3f33f516d70e7ff69fc64db8fc35efe4
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-05-28 18:20:33 +0100

    Fixed with styling, changed color

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from "react";
 import { motion } from "framer-motion";
+import Image from 'next/image'
 
 const Home = () => {
   const [num, setNum] = useState(0);
@@ -17,13 +18,23 @@ const Home = () => {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-slate-600 to-slate-800">
       <motion.button
         onClick={handleClick}
-        className="w-80 h-80 bg-gradient-to-r from-sky-600 to-cyan-600 rounded-full text-white font-bold flex items-center justify-center"
+        className="w-80 h-80 bg-gradient-to-r from-green-600 to-emerald-600 rounded-full text-white font-bold flex items-center justify-center"
         whileHover={{ scale: [null, 1.5, 1.4] }}
         transition={{ duration: 0.3 }}
         whileTap={{ scale: 1.4 }}
       >
         <div className="text-9xl">{num}</div>
       </motion.button>
+
+      <div className="absolute bottom-4 right-4 text-white text-sm">
+      <Image
+          src="/favicon.ico"
+          alt="Luna"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className="absolute bottom-4 left-4 text-white text-xs">Gwy &copy; 2024</div>
     </main>
   );
 };
